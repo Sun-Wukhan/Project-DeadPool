e14d2907f38bbdf3e6a8bf845954d67f8101c94b
Navid Khan <ali.khan@wowinc.com>
2022-09-11 22:25:48 -0400

    Initial

---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <HeroBackground>
+            <VideoBackground src={"videos/background3.mp4"} type="video/mp4" autoPlay loop muted playsInline />
+      </HeroBackground>
+      <ButtonContainer>
+            <MediaButton>Google Login</MediaButton>
+            <MediaButton>Facebook Login</MediaButton>
+            <MediaButton>Twitter Login</MediaButton>
+          </ButtonContainer>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+ height: 100vh; 
+ justify-content: center;
+ align-items: center;
+ display: grid;
+ overflow: hidden;
+ display: flex; 
+`
+
+const HeroBackground = styled.div`
+position: absolute; 
+top: 0; 
+bottom: 0; 
+right: 0; 
+left: 0; 
+height: 100%;
+width: 100%; 
+overflow: hidden;`
+
+const VideoBackground= styled.video`
+position: absolute; 
+height: 100vh; 
+z-index: -10;
+o-object-fit: cover;
+object-fit: cover;
+`
+const ButtonContainer = styled.div`
+ justify-content: center;
+ align-items: center;
+ display: flex;
+ flex-direction: column; 
+`
+const MediaButton = styled.button`
+margin-top: 0.25rem; 
+
+`
